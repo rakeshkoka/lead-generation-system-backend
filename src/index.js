f4168027b6200154493fcab96f6d7b820b4d8a70
Rakesh Koka <rakeshrakhi00344@gmail.com>
2025-05-23 21:20:33 +0530

    added cors function

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import leadRouter from "./routes/lead.route.js";
 dotenv.config();
 
 import cors from "cors";
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://lead-generation-system.vercel.app"
+];
+
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://lead-generation-system.vercel.app/",
-        "https://lead-generation-system-apy60hb18-rakeshs-projects-9f496bf3.vercel.app/"
-    ],
-}))
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 
 //port
 const PORT = process.env.PORT;
